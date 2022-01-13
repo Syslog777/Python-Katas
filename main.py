be0558f418a945dd4a2de8d7708a118d97199107
tito <tcodex250@gmail.com>
2022-01-13 11:42:56 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,34 @@
+
+import requests
+import pandas as pd
+from pandas.io.json import json_normalize
+import json
+import matplotlib.pyplot as plt
+import pandas as pd
+stock_symbols = [
+    'PCRFY',
+    'AMZN',
+    'YUM',
+    'PEP',
+    'BGS',
+    'DELL',
+    'ELED',
+    'SOUND',
+    'SPOT',
+    'WMT',
+    'HUBS',
+    'INTC'
+]
+
+url = "https://alpha-vantage.p.rapidapi.com/query"
+
+querystring = {"interval":"5min","function":"TIME_SERIES_INTRADAY","symbol":"MSFT","datatype":"json","output_size":"compact"}
+
+headers = {
+    'x-rapidapi-host': "alpha-vantage.p.rapidapi.com",
+    'x-rapidapi-key': "c70606e85cmsh84b8593f44d0a70p1b2786jsnc31402d98ec1"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.json())
